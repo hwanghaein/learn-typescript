8620446acad38770916fa68669c5764add1348f5
hwanghaein <hhi9037@naver.com>
2024-02-21 17:43:30 +0900

    기본 타입 - 튜플, 객체, 진위값

--- class-note/1_type-basic.ts
+++ class-note/1_type-basic.ts
@@ -1,7 +1,6 @@
 // JS 문자열 선언
 // const str = "hello";
 
-
 // JS 문자열 선언
 let str: string = "hello";
 
@@ -9,7 +8,27 @@ let str: string = "hello";
 let num: number = 10;
 
 // TS 배열
-let arr: Array<number> = [1,2,3]
-let items: number[] = [1,2,3]; // 다른 방식 (배열 리터럴 방식)
+let arr: Array<number> = [1, 2, 3];
+let items: number[] = [1, 2, 3]; // 다른 방식 (배열 리터럴 방식)
 
-let heroes: Array<string> = ["Capt", "Thor", "Hulk"]
+let heroes: Array<string> = ["Capt", "Thor", "Hulk"];
+
+// TS 튜플 (배열의 모든 index에 type이 정의돼있다)
+let address: [string, number] = ["gangnam", 100];
+
+// TS 객체
+let obj: object = {};
+
+// let person: object = {
+//   name: "capt",
+//   age: 100
+// };
+
+// 객체를 구체적으로 정의하는 법
+let person: { name: string; age: number } = {
+  name: "capt",
+  age: 100,
+};
+
+// TS 진위값
+let show: boolean = true;
