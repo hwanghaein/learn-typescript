b5d4f5efbeab7936baa53a28dc1d1b5b0c7732fa
hwanghaein <hhi9037@naver.com>
2024-02-21 17:53:18 +0900

    함수 타입 - 파라미터를 제한하는 특성

--- class-note/2_functions.ts
+++ class-note/2_functions.ts
@@ -5,14 +5,14 @@
 
 // sum (10, 20);
 
-
-// 함수의 반환 값에 타입을 정의하는 방식 
-function add(): number{
+// 함수의 반환 값에 타입을 정의하는 방식
+function add(): number {
   return 10;
 }
-
 
 // 함수의 타입을 정의하는 방식 (가장 기본적인 모습)
 function sum(a: number, b: number): number {
   return a + b;
 }
+
+
