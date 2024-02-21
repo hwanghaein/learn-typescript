e16cf8905b4b9d2a6113b968346393f333dc4c53
hwanghaein <hhi9037@naver.com>
2024-02-21 16:36:56 +0900

    타입스크립트 라이브러리 설치

---
+++ getting-started/index.js
@@ -0,0 +1,5 @@
+function sum(a, b) {
+    return a + b;
+}
+sum(10, 20);
+// 컴파일(compile): ts파일을 js파일로 변환하는 작업
---
+++ getting-started/index.ts
@@ -0,0 +1,8 @@
+function sum(a: number, b: number): number {
+  return a + b;
+}
+
+sum(10, 20);
+
+// 컴파일(compile): ts파일을 js파일로 변환하는 작업
+
