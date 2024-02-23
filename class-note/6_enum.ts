a9a67343543dc5ce4eadfdcee4fa9cb857b5494b
hwanghaein <hhi9037@naver.com>
2024-02-23 21:58:30 +0900

    문자형 이넘

--- class-note/6_enum.ts
+++ class-note/6_enum.ts
@@ -1,9 +1,8 @@
 enum Shoes {
-  Nike, // 0
-  Adidas, // 1 
+  Nike = "나이키", 
+  Adidas = "아디다스", 
 }
 
 let myShoes = Shoes.Nike;
-console.log(myShoes);  // 0   -> 이넘을 쓸 때, 별도의 값을 지정하지않으면 전부 다 숫자형 이넘으로 취급함 
-
+console.log(myShoes);  // "나이키"
 
