0f16905f58e7ae07817704fb1275c29187522e65
hwanghaein <hhi9037@naver.com>
2024-02-23 19:40:13 +0900

    유니온 타입(Union Type)의 특징

--- class-note/5_operator.ts
+++ class-note/5_operator.ts
@@ -20,4 +20,18 @@
     logMessage(100);
 
 
-    
+    interface Developer{
+      name: string;
+      skill: string;
+    }
+    interface Human{
+      name: string;
+      age: number;
+
+    }
+
+    function askSomeone(someone: Developer | Human) {
+      someone.name // 공통속성만 접근할 수 있음 
+    }
+    // somone에 어떤 속성이 들어올지 모르기 때문에 Developer의 skill이나 Human의 age를 타입 검증도 없이 바로 써버리게 되면, 코드상으로 에러가 날 수도 있다고 여기는거임
+    // 따라서 인터페이스 같은 여러 구조체를 유니온타입으로 쓸 때, 보장된(공통된) 속성에 대해서만 제공해줌
