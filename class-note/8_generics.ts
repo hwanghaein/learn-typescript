dd648a347eebd7a661ba0a2a974059a33c731c01
hwanghaein <hhi9037@naver.com>
2024-03-07 21:22:08 +0900

    제네릭의 기본 문법

---
+++ class-note/8_generics.ts
@@ -0,0 +1,17 @@
+// function logText(text) {
+//   console.log(text);
+//   return text;
+// }
+
+// logText(10); // 숫자 10
+// logText("하이"); // 문자열 하이
+// logText(true); // 진위값 true
+
+
+function logText<T>(text: T):T {
+  console.log(text);
+  return text;
+}
+
+logText<string>("하이"); // 함수를 호출하는 시점에 타입을 넘겨줄 수 있는 것
+
