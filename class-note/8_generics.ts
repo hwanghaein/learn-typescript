f1e1489b4aad391b2bfe89cd87b7e4b979d9afbe
hwanghaein <hhi9037@naver.com>
2024-03-07 21:38:08 +0900

    기존 타입 정의 방식과 제네릭의 차이점 - 유니온 타입을 이용한 선언 방식의 문제점

--- class-note/8_generics.ts
+++ class-note/8_generics.ts
@@ -16,19 +16,31 @@
 // logText<string>("하이"); // 함수를 호출하는 시점에 타입을 넘겨줄 수 있는 것
 
 
-function logText(text: string) {
-  console.log(text);
-  // text.split('').reverse().join('');
-  return text;
-}
+// function logText(text: string) {
+//   console.log(text);
+//   // text.split('').reverse().join('');
+//   return text;
+// }
 
-function logNum(num: number) {
-  console.log(num);
-  return num;
-}
+// function logNum(num: number) {
+//   console.log(num);
+//   return num;
+// }
 
 // logText("a");
 // logText(10); 
 
 
 // 단순히 타입을 다르게 받기 위해서 중복되는 코드를 계속 해서 생산하는건 비효율적임 
+
+
+function logText(text: string | number) {
+  console.log(text);
+  // text. 
+  // string과 number가 공통으로 접근할 수 있는 api만 제공해줌
+  return text;
+}
+
+const a = logText("a"); // const a: string | number
+// a.split('') // Number에 split 속성이 없다고 에러뜸
+logText(10); 
