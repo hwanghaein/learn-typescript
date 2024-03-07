3c8e159b13d846b5fe8f012c595d12d906e58f47
hwanghaein <hhi9037@naver.com>
2024-03-07 22:14:41 +0900

    제네릭의 장점과 타입 추론에서의 이점

--- class-note/8_generics.ts
+++ class-note/8_generics.ts
@@ -34,13 +34,26 @@
 // 단순히 타입을 다르게 받기 위해서 중복되는 코드를 계속 해서 생산하는건 비효율적임 
 
 
-function logText(text: string | number) {
+// function logText(text: string | number) {
+//   console.log(text);
+//   // text. 
+//   // string과 number가 공통으로 접근할 수 있는 api만 제공해줌
+//   return text;
+// }
+
+// const a = logText("a"); // const a: string | number
+// // a.split('') // Number에 split 속성이 없다고 에러뜸
+// logText(10); 
+
+
+function logText<T>(text: T): T {
   console.log(text);
-  // text. 
-  // string과 number가 공통으로 접근할 수 있는 api만 제공해줌
   return text;
 }
 
-const a = logText("a"); // const a: string | number
-// a.split('') // Number에 split 속성이 없다고 에러뜸
-logText(10); 
+const str =  logText<string>("abc") // logText라는 함수의 타입을 string으로 쓰겠다. (인자, 반환값이 모두 string이 될 것)
+str.split('');
+
+const login =  logText<boolean>(true) 
+
+
