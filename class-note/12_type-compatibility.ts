ee500ee7d27b2d7cae7ecaedeec711f5202012f4
황혜인 <hhi9037@naver.com>
2024-04-11 14:45:01 +0900

    타입 호환 예제 - 함수, 제네릭

--- class-note/12_type-compatibility.ts
+++ class-note/12_type-compatibility.ts
@@ -5,19 +5,48 @@ interface Developer{
  skill: string; 
 }
 
-// interface Person {
-//   name: string;
-// }
-
-class Person {
+interface Person {
   name: string;
 }
 
+
 var developer: Developer;
 var person: Person;
-// developer = new Person(); // 에러
 
 // developer = person;  // 에러
 // person = developer; // 이건 가능!
 
-// 타입 호환: 오른쪽에 있는 타입이 더 많은 속성을 갖거나, 구조적으로 더 클 때, 왼쪽과 호환이 된다.
+
+
+// 함수
+var add = function(a: number){
+  // ...
+}
+
+var sum = function(a: number, b: number){ // 위 함수(add)보다 구조가 더 크다.
+  // ...
+}
+
+// add = sum; // 에러
+sum = add; // 이건 가능
+
+
+// 제네릭 
+interface Empty<T> {
+// ..
+}
+var empty1: Empty<string>;
+var empty2: Empty<number>;
+
+// empty1=empty2 // 가능
+// empty2=empty1 // 가능
+
+interface NotEmpty<T>{
+data: T;
+}
+
+var notempty1: NotEmpty<string>;
+var notempty2: NotEmpty<number>;
+
+// notempty1=notempty2 // 에러
+// notempty2=notempty1 // 에러
