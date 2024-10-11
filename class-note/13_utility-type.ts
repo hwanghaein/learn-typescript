87c50425b24519d5f6496fae77feb851bd840339
황혜인 <hhi9037@naver.com>
2024-10-11 18:00:21 +0900

    유틸리티 타입 사례 - Partial

--- class-note/13_utility-type.ts
+++ class-note/13_utility-type.ts
@@ -36,6 +36,21 @@ type ShoppingItem = Pick<Product, 'id'| 'name' | 'price'>
 function displayProductDetail(shoppingItem: ShoppingItem) {
   // shoppingItem에 접근하여 id, name, price를 처리
 }
+// interface UpdateProduct {
+//   id?: number;
+//   name?: string;
+//   price?: number;
+//   brand?: string;
+//   stock?: number;
+// }
 
+// 3. 특정 상품 정보를 업데이트(갱신) 하는 함수
+// 위에 정의한 interface를 이렇게 씀
+// function updateProductItem(productItem: UpdateProduct){
+// }
 
-// 
+// Partial 사용해보기 (Product의 속성을 전부 옵셔널 처리함)
+type UpdateProduct = Partial<Product>
+
+function updateProductItem(productItem: UpdateProduct){
+  }
