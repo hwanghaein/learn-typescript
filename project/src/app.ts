5b7bb6ab776d8c842ae3e13207c4d3a6693ef82d
황혜인 <hhi9037@naver.com>
2024-11-02 23:06:56 +0900

    차트 관련 타입 오류 해결

--- project/src/app.ts
+++ project/src/app.ts
@@ -40,7 +40,6 @@ function createSpinnerElement(id: string) {
 
 // state
 let isDeathLoading = false;
-let isRecoveredLoading = false;
 
 /**
  * @typedef {object} CovidSummary
@@ -58,10 +57,6 @@ let isRecoveredLoading = false;
 /**
  * @returns {Promise<CovidSummary>}
  */
-
-
-
-
 
 
 function fetchCovidSummary(): Promise<AxiosResponse<CovidSummaryResponse>> {
@@ -204,8 +199,9 @@ async function setupData() {
   setLastUpdatedTimestamp(data);
 }
 
-function renderChart(data: any, labels: any) {
-  var ctx = ($("#lineChart") as HTMLCanvasElement).getContext("2d");
+function renderChart(data: number[], labels: string[]) {
+  var lineChart = $("#lineChart") as HTMLCanvasElement;
+  var ctx = lineChart.getContext("2d");
   Chart.defaults.color = "#f5eaea";
   Chart.defaults.font.family = "Exo 2";
   new Chart(ctx, {
@@ -225,11 +221,11 @@ function renderChart(data: any, labels: any) {
   });
 }
 
-function setChartData(data: any) {
-  const chartData = data.slice(-14).map((value: any) => value.Cases);
+function setChartData(data: CountrySummaryResponse) {
+  const chartData = data.slice(-14).map((value: CountrySummaryInfo) => value.Cases);
   const chartLabel = data
     .slice(-14)
-    .map((value: any) =>
+    .map((value: CountrySummaryInfo) =>
       new Date(value.Date).toLocaleDateString().slice(5, -1)
     );
   renderChart(chartData, chartLabel);
